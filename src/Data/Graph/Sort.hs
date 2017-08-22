6f3cf1363de2b3d469d8fefc305c008322c1a75a
Hexirp <Hexirp@users.noreply.github.com>
2017-08-22 12:17:38 +0900

    Add doc

--- src/Data/Graph/Sort.hs
+++ src/Data/Graph/Sort.hs
@@ -1,5 +1,7 @@
+-- | Provide Topological sort for Adjacency list.
 module Data.Graph.Sort where
  import Prelude
 
+ -- | Sort a graph. It's Topological sort.
  tsort :: Ord a => [(a, [a])] -> [(a, [a])]
  tsort = undefined
