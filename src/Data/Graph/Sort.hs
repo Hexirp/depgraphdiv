8f536203d4ea6ab70199df0e9f8f707107df6bf4
Hexirp <Hexirp@users.noreply.github.com>
2017-08-24 22:54:52 +0900

    Update Sort

--- src/Data/Graph/Sort.hs
+++ src/Data/Graph/Sort.hs
@@ -15,31 +15,31 @@ module Data.Graph.Sort where
  copyRef (v, r) = (v, r, r)
 
  -- | Main part of 'tsort'.
- tsort_main :: Eq a => [(a, [a], [a])] -> [(a, [a])]
+ tsort_main :: Eq a => [(a, [a], t)] -> [(a, t)]
  tsort_main [] = []
- tsort_main (x : xs) = let (v, r, r') = x in case r of
-  [] -> (v, r') : (tsort_main $ tsort_sort $ tsort_delete v xs)
+ tsort_main (x : xs) = let (v, r, t) = x in case r of
+  [] -> (v, t) : (tsort_main $ tsort_sort $ tsort_delete v xs)
   _ -> error "Found Loop"
 
  -- | Sort a list of vertex in descending order of the number of vertices referenced.
  --
  -- >>> tsort_sort [(0,[],[]),(1,[2,3],[]),(2,[3],[]),(3,[],[])]
  -- [(0,[],[]),(3,[],[]),(2,[3],[]),(1,[2,3],[])]
- tsort_sort :: Eq a => [(a, [a], [a])] -> [(a, [a], [a])]
+ tsort_sort :: Eq a => [(a, [a], t)] -> [(a, [a], t)]
  tsort_sort = sortOn countRef
 
  -- | Count the number of vertices referring to a vertex.
- countRef :: (a, [a], [a]) -> Int
+ countRef :: (a, [a], t) -> Int
  countRef (_, x, _) = length x
 
  -- | Delete references from a vertex in a graph.
  --
  -- >>> tsort_delete 1 [(0,[],[]),(2,[1,3],[]),(3,[],[])]
  -- [(0,[],[]),(2,[3],[]),(3,[],[])]
- tsort_delete :: Eq a => a -> [(a, [a],[a])] -> [(a, [a],[a])]
+ tsort_delete :: Eq a => a -> [(a, [a], t)] -> [(a, [a], t)]
  tsort_delete x = mapRefs (delete x)
 
  -- | Map a function to a list of reference.
- mapRefs :: ([a] -> [a]) -> [(a, [a], [a])] -> [(a, [a], [a])]
+ mapRefs :: ([a] -> [a]) -> [(a, [a], t)] -> [(a, [a], t)]
  mapRefs f = map (second f) where
   second f (x, y, z) = (x, f y, z)
