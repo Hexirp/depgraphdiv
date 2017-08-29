22bf5c2a6efd3997a89fa1be78311a7398b60f1f
Hexirp <Hexirp@users.noreply.github.com>
2017-08-29 22:55:38 +0900

    Update Mountain

--- src/Data/Graph/Sort.hs
+++ src/Data/Graph/Sort.hs
@@ -1,13 +1,14 @@
--- | Provide Topological sort for Adjacency list.
+-- | Provide Topological sort.
 module Data.Graph.Sort where
  import Prelude
  import Data.List (sortOn, delete)
+ import Control.Arrow (first, second)
 
  -- | 'Pile' is composed of a vertex and vertices referring to it.
  type Pile v = (v, [v])
 
  -- | 'Mountain' is composed of 'Pile' and a tag.
- type Mountain v t = (v, [v], t)
+ type Mountain v t = (Pile v, t)
 
  -- | 'Peak' is a vertex and a tag.
  type Peak v t = (v, t)
@@ -19,14 +20,14 @@ module Data.Graph.Sort where
  tsort :: Eq a => [Pile a] -> [Pile a]
  tsort = tsort_main . tsort_sort . map copyRef
 
- -- | Copy references.
+ -- | Copy references to tags.
  copyRef :: Pile a -> Mountain a [a]
- copyRef (v, r) = (v, r, r)
+ copyRef (v, r) = ((v, r), r)
 
  -- | Main part of 'tsort'.
  tsort_main :: Eq a => [Mountain a t] -> [Peak a t]
  tsort_main [] = []
- tsort_main (x : xs) = let (v, r, t) = x in case r of
+ tsort_main (x : xs) = let ((v, r), t) = x in case r of
   [] -> (v, t) : (tsort_main $ tsort_sort $ tsort_delete v xs)
   _ -> error "Found Loop"
 
@@ -39,16 +40,15 @@ module Data.Graph.Sort where
 
  -- | Count the number of vertices referring to a vertex.
  countRef :: Mountain a t -> Int
- countRef (_, x, _) = length x
+ countRef = length . snd . fst
 
  -- | Delete references from a vertex in a graph.
  --
  -- >>> tsort_delete 1 [(0,[],[]),(2,[1,3],[]),(3,[],[])]
  -- [(0,[],[]),(2,[3],[]),(3,[],[])]
  tsort_delete :: Eq a => a -> [Mountain a t] -> [Mountain a t]
- tsort_delete x = mapRefs (delete x)
+ tsort_delete x = mapRefs $ delete x
 
  -- | Map a function to a list of reference.
  mapRefs :: ([a] -> [a]) -> [Mountain a t] -> [Mountain a t]
- mapRefs f = map (second f) where
-  second f (x, y, z) = (x, f y, z)
+ mapRefs = map . first . second
