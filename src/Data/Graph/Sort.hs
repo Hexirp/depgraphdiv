9975b7a05e06e860e97934359aa3491bee576436
Hexirp <Hexirp@users.noreply.github.com>
2017-09-02 21:28:59 +0900

    Fix functions

--- src/Data/Graph/Sort.hs
+++ src/Data/Graph/Sort.hs
@@ -13,7 +13,7 @@ module Data.Graph.Sort where
  infix 3 :<=
 
  instance Show v => Show (Revadle v) where
-  showsPrec i (v, rs) = showParen (i > prec) $
+  showsPrec i (v :<= rs) = showParen (i > prec) $
    showsPrec (prec + 1) v . showString " :<= " . showsPrec (prec + 1) rs where
     prec = 3
 
@@ -34,40 +34,44 @@ module Data.Graph.Sort where
 
  -- | Sort a graph. It's Topological sort.
  --
- -- >>> tsort [3 <+ [1,2], 2 <+ [0], 1 <+ [0], 0 <+ []]
- -- [(0,[]),(2,[0]),(1,[0]),(3,[1,2])]
+ -- >>> tsort [3 :<= [1,2], 2 :<= [0], 1 :<= [0], 0 :<= []]
+ -- [0 :<= [],2 :<= [0],1 :<= [0],3 :<= [1,2]]
  tsort :: Eq a => Revadl a -> Revadl a
- tsort = tsort_main . tsort_sort . map copyRef
+ tsort = map fromVertext . tsort_main . tsort_sort . map copyRef
+
+ fromVertext :: Vertext a [a] -> Revadle a
+ fromVertext (v, rs) = v :<= rs
 
  -- | Copy references to tags.
  copyRef :: Revadle a -> Revadlet a [a]
- copyRef (v, r) = ((v, r), r)
+ copyRef (v :<= r) = ((v :<= r), r)
 
  -- | Main part of 'tsort'.
  tsort_main :: Eq a => Revadlt a t -> Vertextl a t
  tsort_main [] = []
- tsort_main (x : xs) = let ((v, r), t) = x in case r of
+ tsort_main (x : xs) = let ((v :<= r), t) = x in case r of
   [] -> (v, t) : (tsort_main $ tsort_sort $ tsort_delete v xs)
   _ -> error "Found Loop"
 
  -- | Sort a list of vertex in descending order of the number of vertices referenced.
  --
- -- >>> tsort_sort [((0,[]),[]),((1,[2,3]),[]),((2,[3]),[]),((3,[]),[])]
- -- [((0,[]),[]),((3,[]),[]),((2,[3]),[]),((1,[2,3]),[])]
+ -- >>> tsort_sort [(0 :<= [], []), (1 :<= [2,3], []), (2 :<= [3], []), (3 :<= [], [])]
+ -- [(0 :<= [],[]),(3 :<= [],[]),(2 :<= [3],[]),(1 :<= [2,3],[])]
  tsort_sort :: Eq a => Revadlt a t -> Revadlt a t
  tsort_sort = sortOn countRef
 
  -- | Count the number of vertices referring to a vertex.
  countRef :: Revadlet a t -> Int
- countRef = length . snd . fst
+ countRef ((_ :<= rs), _) = length rs
 
  -- | Delete references from a vertex in a graph.
  --
- -- >>> tsort_delete 1 [((0,[]),[]),((2,[1,3]),[]),((3,[]),[])]
- -- [((0,[]),[]),((2,[3]),[]),((3,[]),[])]
+ -- >>> tsort_delete 1 [(0 :<= [], []), (2 :<= [1,3], []), (3 :<= [], [])]
+ -- [(0 :<= [],[]),(2 :<= [3],[]),(3 :<= [],[])]
  tsort_delete :: Eq a => a -> Revadlt a t -> Revadlt a t
  tsort_delete x = mapRefs $ delete x
 
  -- | Map a function to a list of reference.
  mapRefs :: ([a] -> [a]) -> Revadlt a t -> Revadlt a t
- mapRefs = map . first . second
+ mapRefs = map . mapRev where
+  mapRev f ((v :<= r), t) = ((v :<= f r), t)
