af0ed6ed2a6fe438d9aafb122f98c2e60d66a984
Hexirp <Hexirp@users.noreply.github.com>
2017-09-10 08:02:22 +0900

    Add bench.hs

---
+++ benchmark/bench.hs
@@ -0,0 +1,16 @@
+import Criterion.Main
+
+import Data.Graph.Sort
+
+benchdata_ttsort :: Int -> Revadlet Int ()
+benchdata_ttsort 0 = []
+benchdata_ttsort n = (n :<== ([],())) : benchdata_ttsort (n - 1)
+
+bench_ttsort :: Int -> Revadle Int ()
+bench_ttsort = ttsort . benchdata_ttsort
+
+main :: IO ()
+main = defaultMain [
+ bench "ttsort 100" $ nf bench_ttsort 100,
+ bench "ttsort 200" $ nf bench_ttsort 200,
+ bench "ttsort 300" $ nf bench_ttsort 300]
