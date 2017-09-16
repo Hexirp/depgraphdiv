7c78599af2022f06680e05b76740b017814767ce
Hexirp <Hexirp@users.noreply.github.com>
2017-09-16 16:52:25 +0900

    Clean bench.hs

--- benchmark/bench.hs
+++ benchmark/bench.hs
@@ -8,17 +8,20 @@ import Data.Graph.Sort
 instance (NFData v, NFData t) => NFData (Revadlet v t) where
  rnf (v :<== rt) = deepseq v (deepseq rt ())
 
-benchdata_ttsort :: Int -> Revadlt Int ()
-benchdata_ttsort = map el . flip take [0..] where
+benchdata_full :: Int -> Revadlt Int ()
+benchdata_full = map el . flip take [0..] where
  el n = n :<== (take n [0..], ())
 
-bench_ttsort :: Int -> Revadlt Int ()
-bench_ttsort = ttsort . benchdata_ttsort
+bench_full :: Int -> Revadlt Int ()
+bench_full = ttsort . benchdata_full
+
+benchnfi :: NFData a => (Int -> a) -> Int -> Benchmark
+benchnfi f n = bench (show n) $ nf f n
 
 main :: IO ()
 main = defaultMain [
- bgroup "ttsort" [
-  bench "100" $ nf bench_ttsort 100,
-  bench "200" $ nf bench_ttsort 200,
-  bench "300" $ nf bench_ttsort 300,
-  bench "400" $ nf bench_ttsort 400]]
+ bgroup "full" [
+  benchnfi bench_full 100,
+  benchnfi bench_full 200,
+  benchnfi bench_full 300,
+  benchnfi bench_full 400]]
